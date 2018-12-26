dafb067efd127cc3af235553b91c29e594b279da
Maxim Tolstikov <vadimovich@list.ru>
2018-12-26 14:15:47 +0300

    Установил pod  vk iOS SDK

--- VK/AuthViewController.swift
+++ VK/AuthViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AuthViewController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var loginTextField: UITextField!
